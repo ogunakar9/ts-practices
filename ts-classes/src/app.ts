f33f3496a9cdeaffff2c5be01b9ca54250ddff89
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-05 10:59:56 +0300

    add private modifier and see restrictions

--- ts-classes/src/app.ts
+++ ts-classes/src/app.ts
@@ -1,16 +1,17 @@
 //spread operator on params
-const addNums = (...numbers: number[]) => {
-  return numbers.reduce((curRes, curVal) => {
-    return curRes + curVal;
-  }, 0);
-};
+// const addNums = (...numbers: number[]) => {
+//   return numbers.reduce((curRes, curVal) => {
+//     return curRes + curVal;
+//   }, 0);
+// };
 
-const addedNums = addNums(2, 3, 2.3, 10);
-console.log(addedNums);
+// const addedNums = addNums(2, 3, 2.3, 10);
+// console.log(addedNums);
 
 //class initiation
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -19,10 +20,27 @@ class Department {
   describe(this: Department) {
     console.log("Department:" + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+  }
 }
 
 const accounting = new Department("Accounting");
-console.log(accounting);
+// console.log(accounting);
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-accountingCopy.describe();
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+//cannot push like this due to employees being private
+// accounting.employees[2] = "Anna";
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
