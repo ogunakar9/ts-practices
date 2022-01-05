8b9dc65e3c167f4dacc9959fa2baf82e1173e4f7
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-05 10:50:26 +0300

    add new department class and instantiate it

--- ts-classes/src/app.ts
+++ ts-classes/src/app.ts
@@ -1,3 +1,4 @@
+//spread operator on params
 const addNums = (...numbers: number[]) => {
   return numbers.reduce((curRes, curVal) => {
     return curRes + curVal;
@@ -6,3 +7,22 @@ const addNums = (...numbers: number[]) => {
 
 const addedNums = addNums(2, 3, 2.3, 10);
 console.log(addedNums);
+
+//class initiation
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department:" + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+console.log(accounting);
+
+const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accountingCopy.describe();
