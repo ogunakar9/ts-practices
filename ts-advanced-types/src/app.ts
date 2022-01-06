fc50a6e4e31e300753bb7de7fd2314dc2270c44a
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 14:42:38 +0300

    add function overloads

--- ts-advanced-types/src/app.ts
+++ ts-advanced-types/src/app.ts
@@ -19,8 +19,22 @@ const e1: ElevatedEmployee = {
 
 type Combinable = string | number;
 type Numeric = number | boolean;
-
 type Universal = Combinable & Numeric;
+
+function add(a: string, b: string): string;
+function add(a: number, b: number): number;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+const result = add("mercurial", " opaqueness");
+result.split(" ");
+console.log(result);
 
 type UnknownEmployee = Admin | Employee;
 
