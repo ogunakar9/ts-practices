244e7b466817783bb284491b9132b513e8aa176f
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 17:00:35 +0300

    add generic class

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -39,3 +39,22 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Mason" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("important item");
