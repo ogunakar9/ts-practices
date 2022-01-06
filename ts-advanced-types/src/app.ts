d9bcc04ddc3ea48d9c56bb64498b15caaeb128e7
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 13:51:25 +0300

    type guards

--- ts-advanced-types/src/app.ts
+++ ts-advanced-types/src/app.ts
@@ -21,3 +21,42 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+type UnknownEmployee = Admin | Employee;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  if ("priviledges" in emp) {
+    console.log("Priviledges: ", emp.priviledges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: ", emp.startDate);
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving");
+  }
+  loadCargo(amount: number) {
+    console.log(amount);
+  }
+}
+
+class Car {
+  drive() {
+    console.log("driving");
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1);
+  }
+}
