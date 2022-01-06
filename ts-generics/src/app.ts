e2091a9696cdfd80dd10433e222b868a89a87bed
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 15:45:25 +0300

    built in generic functions

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -1,1 +1,8 @@
-// Code goes here!
+const names: Array<string> = []; //string[]
+names[0].split(" ");
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("hi"), 2000);
+});
+
+promise.then((data) => data.split(" "));
