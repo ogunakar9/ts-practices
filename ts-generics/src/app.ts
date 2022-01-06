dc0436cf379f98a24c6c80932fe3441823997387
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 16:32:43 +0300

    add generic function example

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -13,3 +13,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "max" }, { age: 30 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got only 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements";
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
