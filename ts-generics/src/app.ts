edfb72ab51f69a5e316baaa0e18bbb932c44cc7a
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-06 15:55:36 +0300

    create custom generic function

--- ts-generics/src/app.ts
+++ ts-generics/src/app.ts
@@ -1,8 +1,15 @@
-const names: Array<string> = []; //string[]
-names[0].split(" ");
+// const names: Array<string> = []; //string[]
+// names[0].split(" ");
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => resolve("hi"), 2000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve("hi"), 2000);
+// });
 
-promise.then((data) => data.split(" "));
+// promise.then((data) => data.split(" "));
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "max" }, { age: 30 });
+console.log(mergedObj);
