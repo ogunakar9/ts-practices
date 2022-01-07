f5aa23ae4b5946027696e4e043d8a068d7157d98
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-07 09:53:02 +0300

    add decorator function

--- ts-decorators/src/app.ts
+++ ts-decorators/src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+function Logger(constructor: Function) {
+  console.log("logging...");
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  name = "Max";
+
+  constructor() {
+    console.log("creating person...");
+  }
+}
+
+const pers = new Person();
