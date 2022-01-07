61050aec58fd4c98617683cd3ef1b8feb42d471d
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-07 12:20:06 +0300

    add method, parameter, class, accessor, property decorators

--- ts-decorators/src/app.ts
+++ ts-decorators/src/app.ts
@@ -16,7 +16,7 @@ function WithTemplate(template: string, hookId: string) {
   };
 }
 
-// @Logger("LOGGING PERSON")
+@Logger("LOGGING PERSON")
 @WithTemplate("<h1>My person object<h1>", "app")
 class Person {
   name = "Max";
@@ -27,3 +27,61 @@ class Person {
 }
 
 const pers = new Person();
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Accessor decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Price should be higher than 0");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
