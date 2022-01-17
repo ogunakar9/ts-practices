e398055b2e464733b670a3a535816d1acc090638
Ogun Akar <ogunakar09@gmail.com>
2022-01-17 12:17:24 +0300

    remove .js extension from all imports

--- ts-drag-drop/src/components/project-item.ts
+++ ts-drag-drop/src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Draggable } from "../models/drag-drop.js";
-import { Component } from "./base-component.js";
-import { autoBind } from "../decorators/autobind.js";
-import { Project } from "../models/project.js";
+import { Draggable } from "../models/drag-drop";
+import { Component } from "./base-component";
+import { autoBind } from "../decorators/autobind";
+import { Project } from "../models/project";
 
 // Project item class
 export class ProjectItem
