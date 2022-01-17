74ad7bef4282c4b98ea7903df5cbe207687749bb
Ogun Akar <ogunakar09@gmail.com>
2022-01-17 12:14:32 +0300

    add webpack dependencies and config

--- ts-drag-drop/src/app.ts
+++ ts-drag-drop/src/app.ts
@@ -1,5 +1,5 @@
 import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list";
+import { ProjectList } from "./components/project-list.js";
 
 new ProjectInput();
 new ProjectList("active");
