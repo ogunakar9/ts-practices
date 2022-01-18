5dd802ea16db88eea4afaa0a05e8e6906f4f803e
Ogun Akar <ogunakar09@gmail.com>
2022-01-18 17:45:41 +0300

    add openstreetview map into click event

--- ts-select-share-place/src/app.ts
+++ ts-select-share-place/src/app.ts
@@ -1,9 +1,26 @@
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
+declare var ol: any;
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  const enteredAddress = addressInput.value;
+
+  const coordinates = { lat: 39.925018, lng: 32.836956 }; // Can't fetch coordinates from Google API, use dummy ones
+
+  document.getElementById("map")!.innerHTML = ""; // clear <p> from <div id="map">
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16,
+    }),
+  });
 }
 
 form.addEventListener("submit", searchAddressHandler);
