4c05ca47deb8b8350b1375bffc88ed450f8db667
Ogun Akar <ogunakar09@gmail.com>
2022-01-20 17:14:54 +0300

    add ref to form

--- ts-react-example/src/App.tsx
+++ ts-react-example/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import ToDoList from "./components/ToDoList";
+import NewToDo from "./components/NewToDo";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+  return (
+    <div className="App">
+      <NewToDo />
+      <ToDoList items={todos} />
+    </div>
+  );
+};
 
 export default App;
