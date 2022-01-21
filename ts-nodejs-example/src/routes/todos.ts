cc8327ef28fa7bc68f32758fb93b0c9406e208e3
Ogun Akar <ogunakar09@gmail.com>
2022-01-21 18:46:57 +0300

    nodejs project with ts added, send post req

---
+++ ts-nodejs-example/src/routes/todos.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createTodo } from "../controllers/todos";
+
+const router = Router();
+
+router.get("/");
+
+router.post("/", createTodo);
+
+router.patch("/:id");
+
+router.delete("/:id");
+
+export default router;
